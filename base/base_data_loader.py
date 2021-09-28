5dcff6d0586037c707c74f922a211ed27ac14909
Janez Lapajne <lapajne.janez@gmail.com>
2021-09-28 11:25:31 +0200

    identity check change to is

--- base/base_data_loader.py
+++ base/base_data_loader.py
@@ -4,7 +4,7 @@ class BaseDataLoader():
     def __init__(self, data_handler, shuffle, test_split, random_state, stratify, training):
         dh = data_handler
 
-        if dh.X_data_test == dh.y_data_test == None:
+        if dh.X_data_test is dh.y_data_test is None:
             if 0 < test_split < 1:
                 stratify = dh.y_data if stratify else None
                 X_train, X_test, y_train, y_test = train_test_split(dh.X_data,
@@ -19,7 +19,7 @@ class BaseDataLoader():
                 self.X_out, self.y_out = dh.X_data, dh.y_data
                 print("Whole dataset is used for training.")
 
-        elif dh.X_data_test.all() != None and dh.y_data_test.all() != None:
+        elif dh.X_data_test is not None and dh.y_data_test is not None:
             self.X_out, self.y_out = (dh.X_data, dh.y_data) if training \
                             else (dh.X_data_test, dh.y_data_test)
             print("For training and testing separate datasets configured in data_handler will be used.")
